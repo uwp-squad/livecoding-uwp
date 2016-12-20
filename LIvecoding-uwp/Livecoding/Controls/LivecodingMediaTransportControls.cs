c49f4cfd3b3a7e5cd52e3b99e24116bdc252c17b
Odonno <bottiau.david@laposte.net>
2016-12-20 20:03:31 +0100

    design(stream): create custom media transport controls with header

---
+++ LIvecoding-uwp/Livecoding/Controls/LivecodingMediaTransportControls.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace Livecoding.UWP.Controls
+{
+    public sealed class LivecodingMediaTransportControls : MediaTransportControls
+    {
+        public LivecodingMediaTransportControls()
+        {
+            DefaultStyleKey = typeof(LivecodingMediaTransportControls);
+        }
+    }
+}
