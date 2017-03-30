4bb82699e6e3917b4f8eb940924dc14bdb2f011d
André <andrevdbroro@gmail.com>
2017-03-30 16:52:22 +0200

    Ajout du bouton

---
+++ LIvecoding-uwp/Livecoding/ViewModels/ChatList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using XMPP;
+
+namespace Livecoding.UWP.ViewModels
+{
+    class ChatList : ListViewItem
+    {
+        //Methode
+        private void ChatActivate
+        {
+           
+        }
+    }
+}
